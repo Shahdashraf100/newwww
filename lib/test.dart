8f8557e9d43ec906377c13be7cefaebb27cfc3b5
shahd.ashraf <shahdggg62@gmail.com>
2024-11-07 09:28:22 +0200

    First

---
+++ lib/test.dart
@@ -0,0 +1,31 @@
+void main()
+{
+  int sum(int x,int y)
+  {
+    return x+y;
+  }
+
+  int sub(int x,int y)
+  {
+    return x-y;
+  }
+
+  int mult(int x,int y)
+  {
+    return x * y;
+  }
+  double div(int x,int y)
+  {
+    return x / y;
+  }
+
+  int x=2;
+  int y=3;
+  print(sum(x, y));
+  print(sub(x, y));
+  print(mult(x, y));
+  print(div(x, y));
+
+
+
+}
